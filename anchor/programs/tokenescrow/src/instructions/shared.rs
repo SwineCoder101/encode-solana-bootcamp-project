0540d378b3564c91cd0299f16770d45f42e774f6
Gitter-block <aaron.ong@zoho.com>
2024-12-06 15:39:42 +0800

    Update shared.rs

--- anchor/programs/tokenescrow/src/instructions/shared.rs
+++ anchor/programs/tokenescrow/src/instructions/shared.rs
@@ -1,5 +1,11 @@
+use anchor_lang::prelude::*;
+use anchor_spl::token_interface::TokenAccount;
+
 pub fn transfer_tokens<'info>(
-    from: &InterfaceAccount<'info, TokenAccounts>;
-) ->Result<()>{
-  
-}
+    from: &InterfaceAccount<'info, TokenAccounts>,
+    to: &InterfaceAccount<'info, TokenAccounts>,
+    amount: &u64,
+    mint:&InterfaceAccount<'info,Mint>,
+    authority:&Signer<'info>,
+    token_program: &Interface<'info,TokenInterface>
+) ->Result<()>{}
