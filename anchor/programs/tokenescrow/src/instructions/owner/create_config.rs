e5972e585de73d20900342a6e4f430c7da561567
Liam Faruq <liam@Liams-MacBook-Pro.local>
2024-12-13 16:58:46 +0700

    update-states-for-config

--- anchor/programs/tokenescrow/src/instructions/owner/create_config.rs
+++ anchor/programs/tokenescrow/src/instructions/owner/create_config.rs
@@ -3,17 +3,20 @@ use crate::state::config::{Config, Status};
 
 #[derive(Accounts)]
 pub struct CreateConfig<'info> {
-    #[account(init, payer = user, space = 8 + 32 + 8 + 1)]
+    #[account(init, seeds = [CONFIG_SEED, &escrow_id.to_le_bytes()], bump, payer = authority, space = 8 + 32 + 8 + 1 + 32 + 1 + 8)]
     pub config: Account<'info, Config>,
     #[account(mut)]
-    pub user: Signer<'info>,
+    pub authority: Signer<'info>,
     pub system_program: Program<'info, System>,
 }
 
-pub fn run_create_config(ctx: Context<CreateConfig>) -> Result<()> {
+pub fn create_config(ctx: Context<CreateConfig>, escrow_id: u8, escrow_account: Pubkey, settlement_amount: u64) -> Result<()> {
     let config = &mut ctx.accounts.config;
-    config.owner = *ctx.accounts.user.key;
+    config.authority = *ctx.accounts.authority.key;
     config.creation_timestamp = Clock::get()?.unix_timestamp;
-    config.status = Status::Active;
+    config.status = Status::Pending;
+    config.escrow_account = escrow_account;
+    config.escrow_id = escrow_id;
+    config.settlement_amount = settlement_amount;
     Ok(())
 }
--- anchor/programs/tokenescrow/src/state/config.rs
+++ anchor/programs/tokenescrow/src/state/config.rs
@@ -1,10 +1,13 @@
 use anchor_lang::prelude::*;
 
-#[account]
+#[account(seeds = [CONFIG_SEED, escrow_id])]
 pub struct Config {
-    pub owner: Pubkey,
+    pub authority: Pubkey,
     pub creation_timestamp: i64,
     pub status: Status,
+    pub escrow_account: Pubkey,
+    pub escrow_id: u8,
+    pub settlement_amount: u64,
 }
 
 #[derive(AnchorSerialize, AnchorDeserialize, Clone, Debug, PartialEq)]
